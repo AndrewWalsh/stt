a5b43ea7611200a53ee75358ae7066c3eb1a442d
Andrew <drewwalsh1@gmail.com>
2022-01-25 14:38:14 +0000

    1

--- src/pages/questionOne/questionOne.js
+++ src/pages/questionOne/questionOne.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 
-class QuestionOne extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			label: "I've been clicked: ",
-			counter: 0
-		}
-	}
-	render(){
-		const handleOnClick = () =>{
-			this.setState({
-				counter: this.state.counter + 1
-			})
-		};
-		return (
-			<div style={{marginTop: 48}}>
-				<Button variant="contained" onClick={handleOnClick()}  >
-					{this.state.label} {this.state.counter} times
-				</Button>
-			</div>
-		)
-	}
-}
+const QuestionOne = () => {
+	const [count, setCount] = useState(0)
+	return (
+		<div style={{marginTop: 48}}>
+			<Button variant="contained" onClick={() => setCount(count + 1)}>
+				{`I've been clicked: ${count} times`}
+			</Button>
+		</div>
+	)
+};
 
 export default QuestionOne
